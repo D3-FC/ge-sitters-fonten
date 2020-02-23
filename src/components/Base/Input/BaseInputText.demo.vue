b9cd1ed74df63a8ba31ba450c6a00256719c29b9
D3-FC <d3-fc@mail.ru>
2020-02-23 23:08:20 +0400

    feat(BaseInput): style

---
+++ src/components/Base/Input/BaseInputText.demo.vue
@@ -0,0 +1,28 @@
+<template>
+  <VbDemo>
+    <VbCard title="default">
+      <BaseInputText v-model="text">
+
+      </BaseInputText>
+    </VbCard>
+    <VbCard title="error">
+      <BaseInputText v-model="text" error="AAA">
+
+      </BaseInputText>
+    </VbCard>
+  </VbDemo>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import BaseInputText from './BaseInputText.vue'
+
+@Component({
+  components: {
+    BaseInputText
+  }
+})
+export default class BaseInputTextDemo extends Vue {
+  text = 'aaa'
+}
+</script>
---
+++ src/components/Base/Input/BaseInputText.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-input
+    outlined
+    v-model="valueProxy"
+    dense
+    :error-message="error"
+    :error="!!error"
+    no-error-icon
+  />
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class BaseInputText extends Vue {
+  $refs: any
+
+  @Prop({
+    type: String,
+    required: true
+  }) value!: String
+
+  @Prop(String) error?: string
+
+  get valueProxy () {
+    return this.value
+  }
+
+  set valueProxy (v: any) {
+    this.$emit('input', v)
+  }
+}
+</script>
+
+<style
+  lang="scss"
+  module
+>
+.main {
+
+}
+</style>
