2801ea1e31408001c14c574e6030bf734991036a
D3-FC <d3-fc@mail.ru>
2020-02-23 23:08:06 +0400

    feat(Btn): style

---
+++ src/components/Base/Btn/BaseBtn.demo.vue
@@ -0,0 +1,28 @@
+<template>
+  <VbDemo>
+    <VbCard title="custom width">
+      <BaseBtn style="width: 270px" label="Send">
+
+      </BaseBtn>
+    </VbCard>
+    <VbCard title="">
+      <BaseBtn color="negative" label="Button">
+
+      </BaseBtn>
+    </VbCard>
+  </VbDemo>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import BaseBtn from './BaseBtn.vue'
+
+@Component({
+  components: {
+    BaseBtn
+  }
+})
+export default class BaseBtnDemo extends Vue {
+
+}
+</script>
---
+++ src/components/Base/Btn/BaseBtn.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-btn
+    :label="label"
+    :color="color"
+    unelevated
+    no-caps
+  />
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class BaseBtn extends Vue {
+  $refs: any
+  @Prop(String) label?: string
+  @Prop({
+    type: String,
+    default: 'primary'
+  }) color?: string
+}
+</script>
+
+<style lang="scss" module>
+  .main {
+
+  }
+</style>
